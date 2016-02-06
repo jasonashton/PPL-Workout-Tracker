977f938a283e29cf44dc544031ec52c311cdf881
Jason Ashton <jlashton@wpi.edu>
2016-02-06 12:37:45 -0500

    switched to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160206030236) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "gains", force: :cascade do |t|
     t.string   "name"
     t.string   "day"
