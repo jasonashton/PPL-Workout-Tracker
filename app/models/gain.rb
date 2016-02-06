85efd8fee8305df5258823af914be2a9b784a710
Jason Ashton <jlashton@wpi.edu>
2016-02-06 13:43:05 -0500

    added verification

--- app/models/gain.rb
+++ app/models/gain.rb
@@ -1,3 +1,8 @@
 class Gain < ActiveRecord::Base
-  validates :name, inclusion: { in: %w(Jason David Jacob)}
+
+  validates :name, inclusion: { in: %w(Jason David Jacob)}, presence: true
+  validates :bench, :overhead, :incline, :lateral, :pushdown, :tricep, :shrugs,
+            :row, :pulldown, :seatedrows, :facepull, :curls, :hammercurls,
+            :squats, :legpress, :quads, :hamstring, :calf,
+            numericality: { only_integer: true }, allow_nil: true
 end
