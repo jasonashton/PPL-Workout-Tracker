e8a7a65c9f72e4cc1741eac3e40f7eda34f06b63
Jason Ashton <jlashton@wpi.edu>
2016-02-09 20:01:08 -0500

    Updated forms to be specific to day clicked on or load js accordingly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,29 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-var hideElements = function(){
-  $("#push-form").css("display", "block");
-  $("#pull-form").css("display", "none");
-  $("#leg-form").css("display", "none");
-
-  $('#gain_day').change(function(){
-    var state = $('#gain_day').val();
-    if(state == "Push"){
-      $("#push-form").css("display", "block");
-      $("#pull-form").css("display", "none");
-      $("#leg-form").css("display", "none");
-    }else if(state == "Pull"){
-      $("#push-form").css("display", "none");
-      $("#pull-form").css("display", "block");
-      $("#leg-form").css("display", "none");
-    }else if(state == "Legs"){
-      $("#push-form").css("display", "none");
-      $("#pull-form").css("display", "none");
-      $("#leg-form").css("display", "block");
-    }
-  });
-};
-
-$(document).ready(hideElements);
-$(document).on('page:load', hideElements);
